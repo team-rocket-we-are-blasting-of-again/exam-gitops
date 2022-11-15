424eb8daa81774ae52638a877483fa89a55dc166
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-11-15 16:51:12 +0100

    if statement added

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -8,12 +8,15 @@ sh "scripts/$script.sh"
 
 for root_dir in \
   $curr_dir/modules \
-  $curr_dir/environments
-do
+  $curr_dir/environments; do
   dirs_to_validate=$(find "$root_dir" -mindepth 1 -maxdepth 1 -type d)
   for dir_to_validate in $dirs_to_validate; do
     cd "$dir_to_validate" || exit
     echo "Running $script.sh in $dir_to_validate"
     sh "$curr_dir/scripts/$script.sh"
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+      exit 1
+    fi
   done
 done
