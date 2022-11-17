8b8426e9234453d973554347619a7796c61a3813
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-11-17 12:57:09 +0100

    jdbc connect string

--- environments/staging/camunda.tf
+++ environments/staging/camunda.tf
@@ -44,7 +44,7 @@ resource "kubernetes_deployment" "camunda" {
           }
           env {
             name  = "DB_CONNECTION_STR"
-            value = format("jdbc:postgresql://camunda:5432/%s", var.camunda_postgres_db)
+            value = format("jdbc:postgresql://postgres-camunda-postgresql:5432/%s", var.camunda_postgres_db)
           }
           env {
             name  = "DB_USERNAME"
