79d094e112cdb8a4d2828b5f9f1a49f955e9d694
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-11-29 16:24:54 +0100

    fix: test it with loki-stack

--- environments/devops/monitoring.tf
+++ environments/devops/monitoring.tf
@@ -1,31 +1,39 @@
-resource "helm_release" "kube-prometheus-stack" {
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart = "kube-prometheus-stack"
-  name  = "kube-prometheus-stack"
-  namespace = local.namespace
+resource "helm_release" "loki-stack" {
+  chart = "https://grafana.github.io/helm-charts"
+  name  = "loki-stack"
   values = [
-    file("${path.module}/config/kube-prometheus-stack-values.yaml")
+    file("${path.module}/config/loki-stack-values.yaml")
   ]
-}
-
-resource "helm_release" "promtail" {
-  repository = "https://grafana.github.io/helm-charts"
-  chart = "promtail"
-  name  = "promtail"
   namespace = local.namespace
-  values = [
-    file("${path.module}/config/promtail-values.yaml")
-  ]
 }
-
-resource "helm_release" "loki" {
-  repository = "https://grafana.github.io/helm-charts"
-  chart = "loki-distributed"
-  name  = "loki"
-  namespace = local.namespace
-  set {
-    name  = "gateway.service.port"
-    value = "3101"
-  }
-}
-
+#resource "helm_release" "kube-prometheus-stack" {
+#  repository = "https://prometheus-community.github.io/helm-charts"
+#  chart = "kube-prometheus-stack"
+#  name  = "kube-prometheus-stack"
+#  namespace = local.namespace
+#  values = [
+#    file("${path.module}/config/kube-prometheus-stack-values.yaml")
+#  ]
+#}
+#
+#resource "helm_release" "promtail" {
+#  repository = "https://grafana.github.io/helm-charts"
+#  chart = "promtail"
+#  name  = "promtail"
+#  namespace = local.namespace
+#  values = [
+#    file("${path.module}/config/promtail-values.yaml")
+#  ]
+#}
+#
+#resource "helm_release" "loki" {
+#  repository = "https://grafana.github.io/helm-charts"
+#  chart = "loki-distributed"
+#  name  = "loki"
+#  namespace = local.namespace
+#  set {
+#    name  = "gateway.service.port"
+#    value = "3101"
+#  }
+#}
+#
